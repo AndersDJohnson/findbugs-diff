2a65ea55a74b74a24f1dd50bccc26a4f02f19e04
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-03-03 23:01:40 -0600

    add test with fixtures

--- src/test/groovy/me/andrz/findbugs/diff/FindBugsDiffTest.groovy
+++ src/test/groovy/me/andrz/findbugs/diff/FindBugsDiffTest.groovy
@@ -1,12 +1,37 @@
 package me.andrz.findbugs.diff
 
 import groovy.util.logging.Slf4j
+import org.junit.Test
 
 /**
  *
  */
 @Slf4j
 class FindBugsDiffTest {
+
+    private static final String classPathFixtures = '/me/andrz/findbugs/diff/fixtures'
+    private static final URL case1URLFrom = FindBugsDiffTest.class.getResource(classPathFixtures + '/1/from/findbugs.xml')
+    private static final URL case1URLTo = FindBugsDiffTest.class.getResource(classPathFixtures + '/1/to/findbugs.xml');
+
+    @Test
+    public void testRun() {
+
+        File cwd = new File('.')
+        log.debug(cwd.canonicalPath)
+
+        String pathFrom = case1URLFrom.file
+        String pathTo = case1URLTo.file
+
+        log.debug(pathFrom)
+        log.debug(pathTo)
+
+        FindBugsDiffCli.FindBugsDiffOptions findBugsDiffOptions = new FindBugsDiffCli.FindBugsDiffOptions(
+                pathFrom: pathFrom,
+                pathTo: pathTo
+        )
+
+        FindBugsDiffIo.run(findBugsDiffOptions)
+    }
 
     public static void main(String[] args) {
         String pathFrom = "C:/Users/Anders/code/findbugs-example/findbugs-example-1/target/findbugsXml.xml";
